2209e19185e3eed4f940179f74360d9e81533b69
NMK196 <67795338+NMK196@users.noreply.github.com>
2020-07-10 19:01:27 +0530

    Add files via upload

---
+++ positivenumbers.py
@@ -0,0 +1,14 @@
+list1 = [12,-7,5,64,-14]
+for number in list1:
+    if number > 0:
+       print(number)
+
+print("these are the positive numbers in list1")
+
+list2 = [12,14,-95,3]
+for number in list2:
+    if number >0:
+       print(number)
+
+print("these are the positive numbers in list2")
+
